860007830f95e72e19711351cc0b11feb3d464e1
Blessing Bassey <blessingbassey93@gmail.com>
2026-03-16 09:35:55 +0100

    Updated OverdueUser report

--- src/controller/ReportGenerator.java
+++ src/controller/ReportGenerator.java
@@ -39,15 +39,21 @@ public class ReportGenerator {
 
     public String generateOverdueUsersReport() {
         StringBuilder report = new StringBuilder("Overdue Users Report:\n");
+        boolean found = false;
 
         for (BorrowTransaction transaction : database.getTransactions()) {
             if (transaction.isOverdue()) {
+                found = true;
                 report.append("Borrower ID: ")
                         .append(transaction.getBorrowerId())
                         .append(", Item ID: ")
                         .append(transaction.getItemId())
                         .append("\n");
             }
+        }
+
+        if (!found) {
+            report.append("No overdue users found.\n");
         }
 
         return report.toString();
